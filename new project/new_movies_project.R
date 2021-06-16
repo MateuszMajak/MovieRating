8d42bb353bed81420f6685ed158ea8b3e028bd36
MateuszMajak <mateuszmajak.mm@gmail.com>
2021-06-17 00:48:50 +0200

    add a few changes in the new dataset project (trimming dataset based on target variable)

--- new project/new_movies_project.R
+++ new project/new_movies_project.R
@@ -16,7 +16,7 @@ library(tseries)
 
 
 # directory
-setwd("new project")
+setwd("~/Documents/GitHub/MovieRating/new project")
 
 # Data reading
 df1 <- readRDS("data/tmdb_data1.rds")
@@ -49,7 +49,6 @@ stargazer(
   movies_new, type = "text", 
   summary.stat = c("n", "mean", "sd", "min", "max")
 )
-
 
 colSums(is.na(movies_new)) %>% 
   sort()
@@ -84,6 +83,7 @@ movies_which_train <- createDataPartition(movies_new$vote_average,
 movies_train <- movies_new[movies_which_train,]
 movies_test <- movies_new[-movies_which_train,]
 
+saveRDS(movies_train, 'new_movies_train.rda')
 summary(movies_new$vote_average)
 summary(movies_train$vote_average)
 summary(movies_test$vote_average)
@@ -107,12 +107,18 @@ movies_numeric_vars_order <-
   sort(decreasing = TRUE) %>%
   names()
 
-corrplot.mixed(movies_correlations,
-               upper = "circle",
-               lower = "pie",
-               tl.col="black",
-               tl.pos = "lt",
-               tl.cex = 0.7)
+corrplot.mixed(
+  movies_correlations,
+  upper = "ellipse",
+  lower = "number",
+  tl.pos = "lt",
+  insig = "label_sig",
+  sig.level = c(.001, .01, .05),
+  pch.cex = 0.8,
+  tl.col = "black",
+  number.digits = 3
+) 
+
 
 # Modeling
 #estimation of the model with all variables
@@ -160,7 +166,6 @@ jarque.bera.test(movies_lm2$residuals) # reject h0 so not normal, but sample is 
 
 
 # Predictions
-
 movies_lm2_pred <- predict(movies_lm2, movies_test)
 
 regressionMetrics <- function(real, predicted) {
@@ -179,3 +184,115 @@ regressionMetrics <- function(real, predicted) {
 
 regressionMetrics(real = movies_test$vote_average,
                   predicted = movies_lm2_pred)
+
+# Let's try to clean the data once again
+ggplot(data = movies_train, aes(x = vote_average)) +
+  geom_histogram(aes(y = ..density..)) +
+  stat_function(fun = dnorm,
+                color = 'red',
+                size = 1,
+                args = list(mean = mean(movies_train$vote_average), sd(movies_train$vote_average))) + theme_stata()
+
+# It looks there are many 0 values. It is very likely they are movies without any
+# votes. Let's remove the extreme values 0 and 10.
+movies_trimmed <- movies_new %>% 
+  dplyr::filter(movies_new$vote_average>0 & movies_new$vote_average<10)
+
+# Data division
+set.seed(987654321)
+
+movies_which_train <- createDataPartition(movies_trimmed$vote_average,
+                                          p = 0.7, 
+                                          list = FALSE) 
+
+movies_trimmed_train <- movies_trimmed[movies_which_train,]
+movies_trimmed_test <- movies_trimmed[-movies_which_train,]
+
+# saveRDS(movies_trimmed_train, 'new_movies_trimmed_train.rda')
+summary(movies_trimmed$vote_average)
+summary(movies_trimmed_train$vote_average)
+summary(movies_trimmed_test$vote_average)
+
+# Target variable distribution
+ggplot(data = movies_trimmed_train, aes(x = vote_average)) +
+  geom_histogram(aes(y = ..density..)) +
+  stat_function(fun = dnorm,
+                color = 'red',
+                size = 1,
+                args = list(mean = mean(movies_trimmed_train$vote_average), sd(movies_trimmed_train$vote_average))) + theme_stata()
+
+# vote_average looks a lot better than before and fits the normal distribution better as well
+
+# Correlations
+movies_trimmed_correlations <- 
+  cor(movies_trimmed_train[,movies_numeric_vars],
+      use = "pairwise.complete.obs")
+
+movies_numeric_vars_order <- 
+  movies_trimmed_train[,"vote_average"] %>% 
+  sort(decreasing = TRUE) %>%
+  names()
+
+corrplot.mixed(
+  movies_trimmed_correlations,
+  upper = "ellipse",
+  lower = "number",
+  tl.pos = "lt",
+  insig = "label_sig",
+  sig.level = c(.001, .01, .05),
+  pch.cex = 0.8,
+  tl.col = "black",
+  number.digits = 3
+) 
+
+# We can see the strong correlation between budget and revenue, but also between
+# vote_count and revenue and budget.
+
+# Modeling
+# Now we run the simple linear model again with all variables
+movies_trimmed_lm1 <- lm(vote_average ~ ., 
+                 data = movies_trimmed_train)
+
+summary(movies_trimmed_lm1)
+#R-squared 0.02459 - lower than before
+
+
+# transformation of revenues with yeo-johnson
+yeo_trimmed <- yeojohnson(movies_trimmed_train$revenue)
+
+movies_trimmed_train$revenue_yeo <- yeo_trimmed$x.t
+
+movies_trimmed_test$revenue_yeo <- predict(yeo_trimmed, newdata = movies_trimmed_test$revenue) # applying on test data
+
+
+#estimation with transformed variables
+movies_trimmed_lm2 <- lm(vote_average ~ language_en + adult + vote_count +
+                   runtime + revenue_yeo +log1p(budget) + popularity + years_old, 
+                 data = movies_trimmed_train)
+
+summary(movies_trimmed_lm2)
+#R-squared 0.02502 - slightly better than in the first model
+
+# Diagnostics
+
+## Collinearity
+vif(movies_trimmed_lm2) # no collinearity
+
+## RESET test
+resettest(movies_trimmed_lm2, power=2:3, type="fitted") # linear form is not true, estimators are biased and won't give true values
+
+## Homoscedasticity
+bptest(movies_trimmed_lm2, studentize=TRUE)  # reject h0 -> heteroscedasticity
+
+## distribution of error term
+jarque.bera.test(movies_trimmed_lm2$residuals) # reject h0 so not normal, but sample is huge this time
+
+# Predictions
+movies_trimmed_lm2_pred <- predict(movies_trimmed_lm2, movies_trimmed_test)
+
+regressionMetrics(real = movies_trimmed_test$vote_average,
+                  predicted = movies_trimmed_lm2_pred)
+
+# R^2 is higher (0.083) on the test set, but it is lower than before trimming the dataset
+# (0.179)
+# All error metrics are lower than before the trimming
